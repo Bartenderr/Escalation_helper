7a39bf363121c49678feef9e64e94d7268b9c596
Bartenderr <b.valentino80@gmail.com>
2024-07-15 17:46:11 +0100

    v1 form page created

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, request, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Get form data
+        enrollee_id = request.form['enrollee_id']
+        insurance_plan = request.form['insurance_plan']
+        diagnosis = request.form['diagnosis']
+        drugs = request.form['drugs']
+        investigations = request.form['investigations']
+        procedures = request.form['procedures']
+        room_and_board = request.form['room_and_board']
+
+        # Process the request
+        # ...
+
+        # Render the result page
+        return render_template('result.html', enrollee_id=enrollee_id, insurance_plan=insurance_plan, diagnosis=diagnosis, drugs=drugs, investigations=investigations, procedures=procedures, room_and_board=room_and_board)
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
